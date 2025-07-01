22e33bc02ca84fd7904c2ad24db3af03b52fe5d3
IT21314742 <it21314742@my.sliit.lk>
2025-07-01 10:24:13 +0530

    Update
    Update

--- Simple-agent/main.py
+++ Simple-agent/main.py
@@ -1,3 +1,3 @@
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
-from langchain_mcp_adapters.tools
+from langchain_mcp_adapters.tools import load_mcp_tools
