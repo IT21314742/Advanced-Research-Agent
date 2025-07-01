cc3499caccbc72f0393cfb952319160d43ba919c
IT21314742 <it21314742@my.sliit.lk>
2025-07-01 10:25:55 +0530

    Update
    Update

--- Simple-agent/main.py
+++ Simple-agent/main.py
@@ -2,3 +2,4 @@ from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
+from langchain_openai
