c26afc2761e08188a7fc9ee53f155888410c69da
IT21314742 <it21314742@my.sliit.lk>
2025-08-22 23:23:45 +0530

    Update app
    Update app

--- Simple-agent/main.py
+++ Simple-agent/main.py
@@ -18,4 +18,5 @@ load_dotenv()
 
                                                                 
                                                                             
+
                                                                             
