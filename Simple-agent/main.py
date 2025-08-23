94b7774ecb5a71090625e9ccbd996961173f91c3
IT21314742 <it21314742@my.sliit.lk>
2025-08-23 10:10:02 +0530

    Update App
    Update App

--- Simple-agent/main.py
+++ Simple-agent/main.py
@@ -25,7 +25,8 @@ server_params = StdioServerParameters(
 
 async def main():
     async with stdio_client(server_params) as (read, write):
-        
+        async with ClientSession(read, write) as session:
+            
 
 
 
