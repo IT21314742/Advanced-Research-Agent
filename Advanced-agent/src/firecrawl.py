714c2ce00a1a4d3b65750898dfe017d63324cd25
IT21314742 <it21314742@my.sliit.lk>
2025-08-28 18:54:47 +0530

    backfire Logic configure
    backfire Logic configure

--- Advanced-agent/src/firecrawl.py
+++ Advanced-agent/src/firecrawl.py
@@ -10,4 +10,4 @@ class FirecrawlService:
     def __init__(self):
         api_key = os.getenv("FIRECRAWL_API_KEY")
         if not api_key:
-            raise ValueError("")
+            raise ValueError("Missing FIRECRAWL_API_KEY ")
