dd377e5dcdd565f0b158a0a98838acd5b17e5aed
IT21314742 <it21314742@my.sliit.lk>
2025-08-28 19:07:06 +0530

    format doc
    format doc

--- Advanced-agent/src/firecrawl.py
+++ Advanced-agent/src/firecrawl.py
@@ -12,29 +12,22 @@ class FirecrawlService:
         if not api_key:
             raise ValueError("Missing FIRECRAWL_API_KEY enviroment variable")
         self.app = FirecrawlApp(api_key=api_key)
-        
-        
-    def search_companies(self, query: str,num_results: int = 5):
+
+    def search_companies(self, query: str, num_results: int = 5):
         try:
             result = self.app.search(
                 query=f"{query} company pricing",
                 limit=num_results,
-                scrape_options=V1ScrapeOptions(
-                    formats=["markdown"]
-                )
+                scrape_options=V1ScrapeOptions(formats=["markdown"]),
             )
             return result
         except Exception as e:
             print(e)
-            return[]
-        
-        
+            return []
+
     def scrape_company_pages(self, url: str):
         try:
-            result = self.app.scrape_url(
-                url,
-                formats=["markdown"]
-            )
+            result = self.app.scrape_url(url, formats=["markdown"])
             return result
         except Exception as e:
             print(e)
