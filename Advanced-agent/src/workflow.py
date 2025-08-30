b33df792650534edefec99e0894a63f3d5811929
IT21314742 <it21314742@my.sliit.lk>
2025-08-30 19:23:30 +0530

    import libraries
    import libraries

--- Advanced-agent/src/workflow.py
+++ Advanced-agent/src/workflow.py
@@ -2,4 +2,4 @@ from typing import Dict, Any
 from langgraph.graph import StateGraph, END
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
-from .models import ResearchState, CompanyInfo
+from .models import ResearchState, CompanyInfo, CompanyAnalysis
