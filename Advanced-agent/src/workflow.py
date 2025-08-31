587b14f5bd53d5cae10f86a5e1a6021aa61c11e9
IT21314742 <it21314742@my.sliit.lk>
2025-08-31 08:52:10 +0530

    listing search results
    listing search results

--- Advanced-agent/src/workflow.py
+++ Advanced-agent/src/workflow.py
@@ -34,3 +34,4 @@ class Workflow:
         article_query = f"{state.query} tools comparison best alternatives"
         search_results = self.firecrawl.search_companies(article_query, num_results=3)
         
+        
