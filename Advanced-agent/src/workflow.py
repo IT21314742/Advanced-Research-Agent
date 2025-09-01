264702609c6fa4e73945c8b8598977711812c906
IT21314742 <it21314742@my.sliit.lk>
2025-09-01 14:29:30 +0530

    defining analyzing content
    defining analyzing content

--- Advanced-agent/src/workflow.py
+++ Advanced-agent/src/workflow.py
@@ -64,3 +64,4 @@ class Workflow:
         
     def _analyze_company_content(self, company_name: str, content:str) -> CompanyAnalysis:
         structured_llm = self.llm.with_structured_output(CompanyAnalysis)
+        
