c167a9d7237666c659938254cbfa7907b210335b
IT21314742 <it21314742@my.sliit.lk>
2025-09-01 14:28:40 +0530

    defining analyzing content
    defining analyzing content

--- Advanced-agent/src/workflow.py
+++ Advanced-agent/src/workflow.py
@@ -62,4 +62,5 @@ class Workflow:
         
         
         
-    def _analyze_company_content(self, company_name: str, content:str) -> CompanyAnalysis
+    def _analyze_company_content(self, company_name: str, content:str) -> CompanyAnalysis:
+        
