240cd970be01a70b743157729a8a4f7352d4c098
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-09-15 11:19:45 +0530

    Update
    Update

--- Advanced-agent/src/workflow.py
+++ Advanced-agent/src/workflow.py
@@ -146,5 +146,5 @@ class Workflow:
 
         messages = [
             SystemMessage(content=self.prompts.RECOMMENDATION_SYSTEM),
-            HumanMessage(content=self)
+            HumanMessage(content=self.prompts)
         ]      
