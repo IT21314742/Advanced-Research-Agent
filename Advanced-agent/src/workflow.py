2a85b03d48a064c27c0e75e309507f65c68fd354
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-09-15 11:17:42 +0530

    Update
    Update

--- Advanced-agent/src/workflow.py
+++ Advanced-agent/src/workflow.py
@@ -143,4 +143,5 @@ class Workflow:
         company_data = ",".join([
             company.json() for company in state.companies
         ])                                               
+        
               
