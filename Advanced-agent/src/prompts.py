4c7bbec072f71548bbec2298b23f8fca2988312b
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-09-17 13:10:22 +0530

    Configure new class to developer tools prompts
    Configure new class to developer tools prompts

--- Advanced-agent/src/prompts.py
+++ Advanced-agent/src/prompts.py
@@ -2,4 +2,4 @@ class DeveloperToolPrompts:
     """Collection of prompts for analyzing developer tools and Technologies"""
 
     #Tool extraction prompts
-    TOOL_EXTRACTION_SYSTEM = (
+    TOOL_EXTRACTION_SYSTEM = 
