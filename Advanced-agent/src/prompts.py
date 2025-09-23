16057b277cc04eea50f6c22832603e4fdd602bce
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-09-23 13:33:25 +0530

    Update
    Update

--- Advanced-agent/src/prompts.py
+++ Advanced-agent/src/prompts.py
@@ -11,4 +11,4 @@ class DeveloperToolPrompts:
                  Article Content: {content}
 
 
-                                                        
+                                                            
