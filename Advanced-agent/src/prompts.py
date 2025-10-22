f08d6eb311a844e4d8fc043c1fcc8855764c2054
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-10-22 10:25:08 +0530

    Update prompts.py
    Update prompts.py

--- Advanced-agent/src/prompts.py
+++ Advanced-agent/src/prompts.py
@@ -28,4 +28,4 @@ class DeveloperToolPrompts:
     
 
     # Company/Tool Analysis prompts
-    TOOL_ANALYSIS_SYSTEM = """You are analyzing developer tools and programming"""
+    TOOL_ANALYSIS_SYSTEM = """You are analyzing developer tools and programming technologies."""
