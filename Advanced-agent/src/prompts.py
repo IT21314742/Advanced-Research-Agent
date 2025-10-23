1392aa454dc154409643c1a4e8a8a3cc18c09783
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-10-23 12:12:45 +0530

    Update Prompts
    Update Prompts

--- Advanced-agent/src/prompts.py
+++ Advanced-agent/src/prompts.py
@@ -51,4 +51,5 @@ class DeveloperToolPrompts:
                 Superbase
                 PlanetScale
                 Railway
-                Appwrite)
+                Appwrite
+                Nhost"""
