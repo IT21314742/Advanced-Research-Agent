1121fbb55c94928776da688d279826b349e976c6
IT21314742 <it21314742@my.sliit.lk>
2025-10-30 05:38:33 +0530

    Update Prompts
    Update Prompts

--- Advanced-agent/src/prompts.py
+++ Advanced-agent/src/prompts.py
@@ -77,4 +77,4 @@ class DeveloperToolPrompts:
 
     
     #Recommendation prompts
-    RECOMMENDATIONS_SYSTEM = """You  are a senior software engineer providing quick
+    RECOMMENDATIONS_SYSTEM = """You  are a senior software engineer providing quick 
